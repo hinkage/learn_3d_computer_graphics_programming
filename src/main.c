442c6774e5a45b0109cddff6f89266e10568093e
hinkage <hinkage@qq.com>
2024-05-22 20:49:55 +0800

    Draw lines of a cube.

--- src/main.c
+++ src/main.c
@@ -87,11 +87,16 @@ void update(void) {
 void render(void) {
     for (int i = 0; i < N_MESH_FACES; i++) {
         triangle_t triangle = triangle_to_render[i];
+        // Draw vertices
         for (int j = 0; j < 3; j++) {
             draw_rect(triangle.points[j].x, triangle.points[j].y, 3, 3,
                       0xFFFFFF00);
         }
+        draw_triangle(triangle.points[0].x, triangle.points[0].y,
+                      triangle.points[1].x, triangle.points[1].y,
+                      triangle.points[2].x, triangle.points[2].y, 0xFF00FF00);
     }
+
     render_color_buffer();
     clear_color_buffer(0xFF000000);
     SDL_RenderPresent(renderer);
