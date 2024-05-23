909f5422a69e17914f7623ad9afe933dce557a99
hinkage <hinkage@qq.com>
2024-05-23 11:33:26 +0800

    Painter's algorithm. Bubble sort triangles by average depth in descending order to paint the near one first.

--- src/main.c
+++ src/main.c
@@ -142,21 +142,41 @@ void update(void) {
             projected_points[j].x += (window_width / 2.0);
             projected_points[j].y += (window_height / 2.0);
         }
-        triangle_t projected_triangle = {.points = {{
-                                                        projected_points[0].x,
-                                                        projected_points[0].y,
-                                                    },
-                                                    {
-                                                        projected_points[1].x,
-                                                        projected_points[1].y,
-                                                    },
-                                                    {
-                                                        projected_points[2].x,
-                                                        projected_points[2].y,
-                                                    }},
-                                         .color = mesh_face.color};
+        float avg_depth =
+            (transformed_vertices[0].z + transformed_vertices[1].z +
+             transformed_vertices[2].z) /
+            3.0;
+        triangle_t projected_triangle = {
+            .points = {{
+                           projected_points[0].x,
+                           projected_points[0].y,
+                       },
+                       {
+                           projected_points[1].x,
+                           projected_points[1].y,
+                       },
+                       {
+                           projected_points[2].x,
+                           projected_points[2].y,
+                       }},
+            .color = mesh_face.color,
+            .avg_depth = avg_depth,
+        };
         // Save
         array_push(triangles_to_render, projected_triangle);
+    }
+
+    // Bubble sort big avg_depth to the front of array
+    int num_triangles = array_length(triangles_to_render);
+    for (int i = 0; i < num_triangles; i++) {
+        for (int j = i + 1; j < num_triangles; j++) {
+            if (triangles_to_render[i].avg_depth <
+                triangles_to_render[j].avg_depth) {
+                triangle_t t = triangles_to_render[i];
+                triangles_to_render[i] = triangles_to_render[j];
+                triangles_to_render[j] = t;
+            }
+        }
     }
 }
 
