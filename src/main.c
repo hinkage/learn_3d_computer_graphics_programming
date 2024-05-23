02016d309f30819f4786bad1d09e58e92e51fc49
hinkage <hinkage@qq.com>
2024-05-23 15:39:27 +0800

    Create a World Matrix combining scale, rotation and translation matrices.

--- src/main.c
+++ src/main.c
@@ -109,19 +109,18 @@ void update(void) {
 
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
-            // Scale
+
+            // Create a World Matrix combining scale, rotation and translation
+            // matrices
+            mat4_t world_matrix = mat4_identity();
+            world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+            world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+
             transformed_vertex =
-                mat4_mul_vec4(scale_matrix, transformed_vertex);
-            // Rotate
-            transformed_vertex =
-                mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-            transformed_vertex =
-                mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-            transformed_vertex =
-                mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-            // Translate
-            transformed_vertex =
-                mat4_mul_vec4(translation_matrix, transformed_vertex);
+                mat4_mul_vec4(world_matrix, transformed_vertex);
 
             // Save transformed vertices
             transformed_vertices[j] = transformed_vertex;
